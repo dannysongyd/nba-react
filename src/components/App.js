7b5cee2605dad0d027e973ab5ac711ae2ceca46c
dannysongyd <dannysongyd@gmail.com>
2019-09-28 20:03:23 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../logo.svg'
+import '../styles/App.css'
+import Main from './Main'
+
+import TopNavBar from './TopNavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar />
+      <Main />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/DataViewContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DataViewContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
